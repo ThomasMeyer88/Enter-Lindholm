8cca6bcfb9f33c402ee3dbe56fa53b9206153d26
Thomas Meyer <thomasmmeyer88@gmail.com>
2023-05-17 18:16:22 -0500

    update

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
+const gravity = 0.2;
+
 canvas.width = 1024
 canvas.height = 576
 
@@ -10,23 +12,31 @@ class Sprite {
     constructor({position, velocity}) {
         this.position = position;
         this.velocity = velocity;
+        this.height = 150;
     }
 
     draw() {
         c.fillStyle = 'red';
-        c.fillRect(this.position.x, this.position.y, 50, 150);
+        c.fillRect(this.position.x, this.position.y, 50, this.height);
         console.log('draw');
     }
 
     update() {
         this.draw();
-        this.position.y += 10;
+        this.position.y += this.velocity.y;
+
+        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+            console.log(`check`);
+            this.velocity.y = 0;
+        } else {
+            this.velocity.y += gravity;
+        }
     }
 }
 
 const player = new Sprite({
     position: { x: 0, y: 0 },
-    velocity: { x: 0, y: 0 }
+    velocity: { x: 0, y: 10 }
 });
 
 const enemy = new Sprite({
@@ -40,6 +50,8 @@ enemy.draw();
 
 function animate() {
     window.requestAnimationFrame(animate);
+    c.fillStyle = 'black';
+    c.fillRect(0, 0, canvas.width, canvas.height);
     player.update();
     enemy.update();
 }
